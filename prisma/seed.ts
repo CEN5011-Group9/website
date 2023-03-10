d69d9ac14fe7e35aeb121e303e1bfd979861e700
Joseph R. Quinn <423821+quinnjr@users.noreply.github.com>
2023-03-10 07:38:34 -0500

    Adding Database seed, wiring up Password module

---
+++ prisma/seed.ts
@@ -0,0 +1,54 @@
+import { PrismaClient, UserRole } from '@prisma/client';
+import argon2 from 'argon2';
+
+const prisma = new PrismaClient();
+
+async function seed() {
+  const passwordUser = await argon2.hash('Password!');
+
+  let user: any = {
+    email: 'test@website.com',
+    passwordHash: passwordUser,
+    role: UserRole.User,
+  }
+
+  user = await prisma.user.upsert({
+    where: {
+      email: user.email
+    },
+    create: user,
+    update: user
+  });
+
+  const passwordClub = await argon2.hash('Password!');
+
+  let userClub: any = {
+    email: 'club@website.com',
+    passwordHash: passwordClub,
+    role: UserRole.ClubOwner
+  }
+
+  userClub = await prisma.user.upsert({
+    where: {
+      email: userClub.email
+    },
+    create: userClub,
+    update: userClub
+  });
+
+  const passwordAdmin = await argon2.hash('Password!');
+
+  let userAdmin: any = {
+    email: 'admin@website.com',
+    passwordHash: passwordAdmin,
+    role: UserRole.Admin
+  }
+
+  userAdmin = await prisma.user.upsert({
+    where: {
+      email: userAdmin.email
+    },
+    create: userAdmin,
+    update: userAdmin
+  })
+}
--- server/app.module.ts
+++ server/app.module.ts
@@ -1,14 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AngularUniversalModule } from '@nestjs/ng-universal';
+import { ConfigModule } from '@nestjs/config';
 import { join } from 'path';
+
 import { AppServerModule } from '../src/main.server';
+
+import { AuthModule } from './auth/auth.module';
+
+import { DatabaseService } from './database/database.service';
 
 @Module({
   imports: [
     AngularUniversalModule.forRoot({
       bootstrap: AppServerModule,
       viewsPath: join(process.cwd(), 'dist/website/browser')
-    })
+    }),
+    ConfigModule.forRoot(),
+    AuthModule
+  ],
+  providers: [
+    DatabaseService
   ]
 })
 export class AppModule {}
